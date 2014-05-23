602ae03d1c72d747f0cd5e60c03fbe3a59546ba8
Olmo Zavala <olmozavala@gmail.com>
2014-05-23 12:31:55 -0400

    Change gif images to png for all layers

--- web/layers/ChangeTxtOnFiles.sh
+++ web/layers/ChangeTxtOnFiles.sh
@@ -1,4 +1,5 @@
 
 #!/bin/sh
-find . -name '*.xml' -exec sed -i "s/viewer.coaps.fsu.edu\/ncWMS/146.201.212.61\/ncWMS/g" '{}' \;
+#find . -name '*.xml' -exec sed -i "s/viewer.coaps.fsu.edu\/ncWMS/146.201.212.61\/ncWMS/g" '{}' \;
+find . -name '*.xml' -exec sed -i "s/image\/gif/image\/png/g" '{}' \;
 
